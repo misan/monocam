5e9e94a8791dfb2f57cbb880d5ba745c4b30af25
anders.e.e.wallin <anders.e.e.wallin@7dcb2d4e-2635-0410-ab65-8977ca58466f>
2007-07-28 12:09:24 +0000

    some more dropcutter code (untested, but compiles)

--- Project/src/cam/DropCutter.cs
+++ Project/src/cam/DropCutter.cs
@@ -209,8 +209,200 @@ namespace monoCAM
         public static double? EdgeTest(Cutter cu, Geo.Point e, Geo.Point p1, Geo.Point p2)
         { 
             // contact cutter against edge from p1 to p2
+
+            // translate segment so that cutter is at (0,0)
+            Geo.Point start = new Geo.Point(p1.x - e.x, p1.y - e.y, p1.z);
+            Geo.Point end = new Geo.Point(p2.x - e.x, p2.y - e.y, p2.z);
+
+            // find angle btw. segment and X-axis
+            double dx = end.x - start.x;
+            double dy = end.y - start.y;
+            double alfa;
+            if (dx != 0)
+                alfa = Math.Atan(dy / dx);
+            else
+                alfa = Math.PI / 2;
+
+            // rotation matrix for rotation around z-axis:
+            // should probably implement a matrix class later
+
+            // rotate by angle alfa
+            start.x = start.x * Math.Cos(alfa) + start.y * Math.Sin(alfa);
+            start.y = -start.x * Math.Sin(alfa) + start.y * Math.Cos(alfa);
+            end.x = end.x * Math.Cos(alfa) + end.y * Math.Sin(alfa);
+            end.y = -end.x * Math.Sin(alfa) + end.y * Math.Cos(alfa);
+
+            // check if segment is below cutter
+            if (start.y > 0)
+            {   // if it's above cutter then rotate some more
+                alfa = alfa + Math.PI;
+                start.x = start.x * Math.Cos(alfa) + start.y * Math.Sin(alfa);
+                start.y = -start.x * Math.Sin(alfa) + start.y * Math.Cos(alfa);
+                end.x = end.x * Math.Cos(alfa) + end.y * Math.Sin(alfa);
+                end.y = -end.x * Math.Sin(alfa) + end.y * Math.Cos(alfa);
+            }
+
+            double l = -start.y; // distance from cutter to edge
+
+            // now we have two different algorithms depending on the cutter:
+            if (cu.r == 0)
+            {
+                // this is the flat endmill case
+                // it is easier and faster than the general case, so we handle it separately
+                if (l > cu.R) // edge is outside of the cutter
+                    return null;
+                else // we are inside the cutter
+                {   // so calculate CC point
+                    double xc1 = Math.Sqrt(Math.Pow(cu.R, 2) - Math.Pow(l, 2));
+                    double xc2 = -xc1;
+                    double zc1 = ((xc1 - start.x) / (end.x - start.x)) * (end.z - start.z) + start.z;
+                    double zc2 = ((xc2 - start.x) / (end.x - start.x)) * (end.z - start.z) + start.z;
+
+                    // choose the higher point
+                    double zc,xc;
+                    if (zc1 > zc2)
+                    {
+                        zc = zc1;
+                        xc = xc1;
+                    }
+                    else
+                    {
+                        zc = zc2;
+                        xc = xc2;
+                    }
+
+                    // now that we have a CC point, check if it's in the edge
+                    if ((start.x > xc) && (xc < end.x))
+                        return null;
+                    else if ((end.x < xc) && (xc > start.x))
+                        return null;
+                    else
+                        return zc;
+
+                }
+                // unreachable place (according to compiler)
+            } // end of flat endmill (r=0) case
+
+            else if (cu.r > 0)
+            { 
+                // this is the general case (r>0)   ball-nose or bull-nose (spherical or toroidal)
+                // later a separate case for the ball-cutter might be added (for performance)
+
+                double xd=0, w=0, h=0, xd1=0, xd2=0, xc=0 , ze=0, zc=0;
+
+                if (l > cu.R) // edge is outside of the cutter
+                    return null;
+                else if (((cu.R-cu.r)<l)&&(l<=cu.R))
+                {    // toroidal case
+                    xd=0; // center of ellipse
+                    w=Math.Sqrt(Math.Pow(cu.R,2)-Math.Pow(l,2)); // width of ellipse
+                    h=Math.Sqrt(Math.Pow(cu.r,2)-Math.Pow((l-(cu.R-cu.r)),2)); // height of ellipse
+                }
+                else if ((cu.R-cu.r)>=l)
+                {
+                    // quarter ellipse case
+                    xd1=Math.Sqrt( Math.Pow((cu.R-cu.r),2)-Math.Pow(l,2));
+                    xd2=-xd1;
+                    h=cu.r; // ellipse height
+                    w=Math.Sqrt( Math.Pow(cu.R,2)-Math.Pow(l,2) )- Math.Sqrt( Math.Pow((cu.R-cu.r),2)-Math.Pow(l,2) ); // ellipse height
+                }
+
+                // now there is a special case where the theta calculation will fail if
+                // the segment is horziontal, i.e. start.z==end.z  so we need to catch that here
+                if (start.z==end.z)
+                {
+                    if ((cu.R-cu.r)<l) 
+                    {
+                        // half-ellipse case
+                        xc=0;
+                        h=Math.Sqrt(Math.Pow(cu.r,2)-Math.Pow((l-(cu.R-cu.r)),2));
+                        ze = start.z + h - cu.r;
+                    }
+                    else if ((cu.R - cu.r) > l)
+                    {
+                        // quarter ellipse case
+                        xc = 0;
+                        ze = start.z;
+                    }
+
+                    // now we have a CC point
+                    // so we need to check if the CC point is in the edge
+                    if ((start.x > xc) && (xc < end.x))
+                        return null;
+                    else if ((end.x < xc) && (xc > start.x))
+                        return null;
+                    else
+                        return ze;
+
+                } // end horizontal edge special case
+
+
+                // now the general case where the theta calculation works
+                double theta = Math.Atan( h*(start.x-end.x)/(w*(start.z-end.z))  );
+
+                // based on this calculate the CC point
+                if (((cu.R - cu.r) < l) && (cu.R <= l))
+                {
+                    // half-ellipse case
+                    double xc1 = xd + Math.Abs(w * Math.Cos(theta));
+                    double xc2 = xd - Math.Abs(w * Math.Cos(theta));
+                    double zc1 = ((xc1 - start.x) / (end.x - start.x)) * (end.z - start.z) + start.z;
+                    double zc2 = ((xc2 - start.x) / (end.x - start.x)) * (end.z - start.z) + start.z;
+                    // select the higher point:
+                    if (zc1 > zc2)
+                    {
+                        zc = zc1;
+                        xc = xc1;
+                    }
+                    else
+                    {
+                        zc = zc2;
+                        xc = xc2;
+                    }
+
+                }
+                else if ((cu.R - cu.r) > l)
+                { 
+                    // quarter ellipse case
+                    double xc1 = xd1 + Math.Abs(w * Math.Cos(theta));
+                    double xc2 = xd2 - Math.Abs(w * Math.Cos(theta));
+                    double zc1 = ((xc1 - start.x) / (end.x - start.x)) * (end.z - start.z) + start.z;
+                    double zc2 = ((xc2 - start.x) / (end.x - start.x)) * (end.z - start.z) + start.z;
+                    // select the higher point:
+                    if (zc1 > zc2)
+                    {
+                        zc = zc1;
+                        xc = xc1;
+                    }
+                    else
+                    {
+                        zc = zc2;
+                        xc = xc2;
+                    }
+                }
+
+                // now we have a valid xc value, so calculate the ze value:
+                ze = zc + Math.Abs(h * Math.Sin(theta)) - cu.r;
+
+                // finally, check that the CC point is in the edge
+                if ((start.x > xc) && (xc < end.x))
+                    return null;
+                else if ((end.x < xc) && (xc > start.x))
+                    return null;
+                else
+                    return ze;
+
+
+                // this line is unreachable (according to compiler)
+                
+            } // end of toroidal/spherical case
+ 
+            
+            // if we ever get here it is probably a serious error!
+            System.Console.WriteLine("EdgeTest: ERROR: no case returned a valid ze!");
             return null;
-        }
+
+        } // end of EdgeTest method
 
 
 
